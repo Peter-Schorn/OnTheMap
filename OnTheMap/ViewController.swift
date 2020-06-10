5519eb55054b00b8a81a3395e27b3d81269bb789
Peter-Schorn <58197311+Peter-Schorn@users.noreply.github.com>
2020-06-10 15:48:22 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Peter Schorn on 6/10/20.
+//  Copyright © 2020 Peter Schorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
